1e101e680405c4991be604bb2693cafbdf6d8c8a
Luckyjamess <6106021630023@fitm.kmutnb.ac.th>
2021-04-26 18:40:56 +0700

    fix port

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const env = require('dotenv')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require('cors') 
+const PORT = process.env.PORT || 3003
 app.use(cors())  
 app.use((req, res, next) => {     let ALLOW_ORIGIN = ['domain-a.com', 'domain-b.com', 'domain-c.com']     
     let ORIGIN = req.headers.origin        
@@ -12,7 +13,7 @@ app.use((req, res, next) => {     let ALLOW_ORIGIN = ['domain-a.com', 'domain-b.
         res.header('Access-Control-Allow-Methods','POST, GET, PUT, PATCH, DELETE, OPTIONS')        
         res.header('Access-Control-Allow-Headers','Content-Type, Option, Authorization')        
         return next()     })
-        
+
 env.config()
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.qgh9i.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`,
     {
@@ -24,6 +25,6 @@ mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONG
 })
 app.use(express.json())
 app.use('/api',projectADV)
-app.listen(process.env.PORT,() =>{ 
-    console.log('server running ...', process.env.PORT);
+app.listen(PORT,() =>{ 
+    console.log('server running ...'+PORT);
 })
